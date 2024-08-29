eb6b90a29dcb80fba5cd15af86efeb670c0e8950
rushitazignuts <rushitav@zignuts.com>
2024-08-29 15:14:44 +0530

    add navigation scroll

--- js/script.js
+++ js/script.js
@@ -14,3 +14,21 @@ document.querySelector(".navbar-toggler").addEventListener("click", function() {
         navbar.classList.add("scrolled"); // Always add background color when menu opens
     }
 });
+
+//  js for navigation link scroll into section wise
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuItems = document.querySelectorAll("a[data-section]");
+  
+    menuItems.forEach((item) => {
+      item.addEventListener("click", function (event) {
+        event.preventDefault();
+        const sectionName = this.getAttribute("data-section");
+        const element = document.getElementById(sectionName);
+  
+        if (element) {
+          element.scrollIntoView({ behavior: "smooth" });
+        }
+      });
+    });
+  });
